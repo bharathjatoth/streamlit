cd64301c9b96997f7154e4eb344d89700213f25d
Kittu <bharath72kumar@gmail.com>
2023-09-19 09:28:45 +0530

    Create basic_streamlit.py

---
+++ basic_streamlit.py
@@ -0,0 +1,36 @@
+import streamlit as st
+from langchain.callbacks.base import BaseCallbackHandler
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import ChatMessage
+
+class StreamHandler(BaseCallbackHandler):
+    def __init__(self,container,initial_text=""):
+        self.container = container
+        self.text = initial_text
+
+    def on_llm_new_token(self,token: str,**kwargs) -> None:
+        self.text += token
+        self.container.markdown(self.text)
+
+with st.sidebar:
+    open_ai_key = st.text_input("Please enter a valid OPEN_AI_KEY",type="password")
+
+if "messages" not in st.session_state:
+    st.session_state['messages'] = [ChatMessage(role="Assistant",content="Hi I am your new assistant")]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg.role).write(msg.content)
+
+if prompt:= st.chat_input():
+    st.session_state.messages.append(ChatMessage(role='User',content='prompt'))
+    st.chat_message("user").write(prompt)
+
+    if not open_ai_key:
+        st.info("please enter a valid open api key")
+        st.stop()
+
+    with st.chat_message('assistant'):
+        streamhandler = StreamHandler(st.empty())
+        llm = ChatOpenAI(open_api_key=open_ai_key,streaming=True,callbacks=[streamhandler])
+        response = llm(st.session_state.messages)
+        st.session_state.messages.append(ChatMessage(role="Assistant",content=response.content))
